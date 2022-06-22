b33d53e1635ebebcfea4f2c2d09b1eea2053975a
MarcinGladkowski <marcingladkowski@gmail.com>
2022-06-22 17:33:19 +0200

    check is excluded

--- workflows/scan-cv-workflow/check-exclude-list/handler.ts
+++ workflows/scan-cv-workflow/check-exclude-list/handler.ts
@@ -1,20 +1,26 @@
 import { ScanCvEvent } from "../types";
 import S3 from "aws-sdk/clients/s3";
 import { CheckExcludeListsLambdaEnvironmentVariables } from "./config";
+import { winstonLogger as logger } from "../../../shared/logger";
+
 
 export const checkExcludeLists = async (
   event: ScanCvEvent,
   config: CheckExcludeListsLambdaEnvironmentVariables,
   s3Client: S3,
 ) => {
-  const isExcluded = false;
-
   const { Body: body } = await s3Client
   .getObject({
     Bucket: config.extractedFilesBucketName,
     Key: event.key,
   })
   .promise();  
+
+  const parsedBody = body!.toString().toLocaleLowerCase();
+
+  const isExcluded = (parsedBody.includes('janusz') && parsedBody.includes('kowalski') && parsedBody.includes("chrzaszczyzewoszyce"));
+
+  logger.info("Handling event: %o", event);
 
   return {
     ...event,
